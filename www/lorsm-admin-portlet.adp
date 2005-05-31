1b7380957d2c184693addf19a27a2030e2906851
Miguel Marin <miguelmmarin@viaro.net>
2005-05-31 22:56:08 +0000

    new functionallities to interact with lors-central

--- www/lorsm-admin-portlet.adp
+++ www/lorsm-admin-portlet.adp
@@ -1,3 +1,17 @@
+<if @use_lc_p@>
+  <ul>
+    <li>
+      <a href="@lors_central_url@">#lorsm-portlet.Manage_LORS#</a>
+    </li>
+    <li>
+      <a href="@lors_central_url@course-add">#lorsm-portlet.Add_a_Course#</a>
+    </li>
+    <li>
+      <a href="/search">#lorsm-portlet.lt_Search_Learning_Objec#</a>
+    </li>
+  </ul>
+</if>
+<else>
 <ul>
   <li>
     <a href="@url@">#lorsm-portlet.Manage_LORS#</a>
@@ -9,4 +23,4 @@
     <a href="/search">#lorsm-portlet.lt_Search_Learning_Objec#</a>
   </li>
 </ul>
-
+</else>
--- www/lorsm-admin-portlet.tcl
+++ www/lorsm-admin-portlet.tcl
@@ -10,6 +10,17 @@ ad_page_contract {
     
 }
 
+
+set lors_central_package_id [apm_package_id_from_key "lors-central"]
+set lors_central_url [apm_package_url_from_id $lors_central_package_id]
+
+if { ![string equal $lors_central_package_id 0] } {
+    set use_lc_p [parameter::get -parameter "UploadUsingLorsCentralP" -package_id $lors_central_package_id]
+} else {
+    set use_lc_p 0
+}
+
+
 array set config $cf
 set list_of_package_ids $config(package_id)
 
@@ -23,3 +34,4 @@ set package_id [lindex $list_of_package_ids 0]
 set url [lindex [site_node::get_url_from_object_id -object_id $package_id] 0]
 
 ad_return_template 
+
